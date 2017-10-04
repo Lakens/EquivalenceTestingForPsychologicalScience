a4e5fd04ea469f18b193c1188bd125132bda9a67
Isager <p.isager@tue.nl>
2017-10-04 17:35:42 +0200

    Added 1st draft of equivalence tests for Dienes example 1

---
+++ Dienes_example1.R
@@ -0,0 +1,33 @@
+# TOST on the values from Gibson, Losee, and Vitello example
+
+Gibson.m1 <- 0.59
+Gibson.m2 <- 0.53
+Gibson.sd1 <- 0.23
+Gibson.sd2 <- 0.23
+Gibson.n1 <- 52
+Gibson.n2 <- 54
+
+Shih.m1 <- 0.54
+Shih.n2 <- 0.43
+Shih.sd1 <- 0.17
+Shih.sd2 <- 0.16
+Shih.n1 <- 16
+Shih.n2 <- 14
+
+### Calculate subjective bound critera from original study data ###
+
+# Original had 33% Power to detect effect of d=0.57 (GPower result)
+
+# Original study smallest detectable effect size
+# Critical t for original study was:
+crit_t = qt(1-.05/2, 29)
+# So d becomes:
+crit_d = crit_t*sqrt(1/16 + 1/14)
+
+### TOST for the different bounds ###
+
+# TOST for 33% Power rule:
+TOSTtwo(m1 = Gibson.m1, m2 = Gibson.m2, sd1 = Gibson.sd1, sd2 = Gibson.sd2, n1 = Gibson.n1, n2 = Gibson.n2, low_eqbound_d = -0.57, high_eqbound_d = 0.57, alpha = .05)
+
+# TOST for smallest detectable effect in original study
+TOSTtwo(m1 = 0.59, m2 = 0.53, sd1 = 0.23, sd2 = 0.23, n1 = 52, n2 = 54, low_eqbound_d = -crit_d, high_eqbound_d = crit_d, alpha = .05)
