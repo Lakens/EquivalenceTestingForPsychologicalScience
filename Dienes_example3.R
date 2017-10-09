d78d06e85f409a516b83b2f1f6258dcd8fd7905f
Scheel <a.m.scheel@tue.nl>
2017-10-09 20:09:18 +0200

    fixed error in 33% power calculation

--- Dienes_example3.R
+++ Dienes_example3.R
@@ -355,7 +355,7 @@ d.crit = t.crit*sqrt((1/(orig.N/2)) + 1/(orig.N/2))
 raw.crit <- d.crit * sqrt(orig.sd1^2 + orig.sd2^2/2) # smallest raw effect orig. study could detect
 
 # Small telescopes: Calculate d for which orig. study had 33% power
-power <- pwr.t.test(n = orig.N, d = NULL, sig.level = 0.05, power = 0.33, type = "two.sample", alternative = "two.sided")
+power <- pwr.t.test(n = orig.N/2, d = NULL, sig.level = 0.05, power = 0.33, type = "two.sample", alternative = "two.sided")
 d.33 <- power$d
 raw.33 <- d.33 * sqrt(orig.sd1^2 + orig.sd2^2/2) # raw effect for which orig. had 33% power
 
