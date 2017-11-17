c0ac69f38ad202a91000a3ce933631f417f3f861
Isager <p.isager@tue.nl>
2017-11-17 10:53:26 +0100

    Added library(TOSTER) to .R examples
    * Minor comment edits
    * Minor formatting edits

--- Example1_MoonRoeder.R
+++ Example1_MoonRoeder.R
@@ -1,32 +1,30 @@
+# Load TOSTER into R environment 
+library(TOSTER)
+
 #  Load the relevant datafile into R
-moon.data        <- read.csv("Example1_MoonRoeder_data-cleaned.csv")  # Data can be found at https://osf.io/rwv5s/ (final_dataset_139_to_send.sav)
+moon.data <- read.csv("Example1_MoonRoeder_data-cleaned.csv")  # Data can be found at https://osf.io/rwv5s/ (final_dataset_139_to_send.sav)
 
 #  Set the bounds, in this case 1 grade point, which corresponds to 6.25%
 sesoi <- 0.0625
 
 #  Calculate the summary statistics from the data that we need in the TOST calculation below
-mean.asian   <- mean(moon.data$accuracy[moon.data$identity_salience == 1])
+mean.asian <- mean(moon.data$accuracy[moon.data$identity_salience == 1])
 mean.control <- mean(moon.data$accuracy[moon.data$identity_salience == 2])
-sd.asian         <- sd(moon.data$accuracy[moon.data$identity_salience == 1])
-sd.control   <- sd(moon.data$accuracy[moon.data$identity_salience == 2])
-n.asian          <- sum(moon.data$identity_salience == 1)
-n.control        <- sum(moon.data$identity_salience == 2)
+sd.asian <- sd(moon.data$accuracy[moon.data$identity_salience == 1])
+sd.control <- sd(moon.data$accuracy[moon.data$identity_salience == 2])
+n.asian <- sum(moon.data$identity_salience == 1)
+n.control <- sum(moon.data$identity_salience == 2)
 sample.sd <- sqrt((sd.asian^2 + sd.control^2) / 2)  # Calculate sd root mean squared for Welch's t-test
 sample.se <- sqrt(sd.asian^2 / n.asian + sd.control^2 / n.control)  # Calculate the se of the difference based on sd.pooled
 
 df <- (sd.asian^2 / n.asian + sd.control^2 / n.control)^2 / (((sd.asian^2 / n.asian)^2 / (n.asian-1)) + ((sd.control^2 / n.control)^2 / (n.control - 1)))  # Degrees of freedom for Welch's t-test
-
 t <- (mean.asian - mean.control) / sample.se  # Welch t-test
-
 p <- 2 * pt(t, df)
-
 sdpooled <- sqrt((((n.asian - 1) * (sd.asian^2)) + (n.control - 1) * (sd.control^2)) / ((n.asian + n.control) - 2))
 d <- (mean.asian - mean.control) / sdpooled
 
 
-
-#  We can now run the TOST!
-#  Performing the TOST using the 'TOSTtwo.raw' function allows you to specify equivalence bounds in raw values
+#  Performing the equivalence test
 Moon <- TOSTER::TOSTtwo.raw(m1  = mean.asian,
                             m2  = mean.control,
                             sd1 = sd.asian,
--- Example4_Lynott-et-al.R
+++ Example4_Lynott-et-al.R
@@ -1,27 +1,23 @@
-se <- sqrt((0.75 * (1 - 0.75)) / 26 + (0.46 * (1 - 0.46)) / 27)  # Calculate the (unpooled) standard error
-crit.z <- qnorm(1 - (0.05 / 2))  # Determine the critical z for a two-tailed test with alpha = 0.05
+# Load TOSTER
+library(TOSTER)
 
+# Calculate the smallest effect sizer of interest (crit.diff), used for setting the bounds.
+se <- sqrt((0.75 * (1 - 0.75)) / 26 + (0.46 * (1 - 0.46)) / 27)  # Calculate the (unpooled) standard error.
+crit.z <- qnorm(1 - (0.05 / 2))  # Determine the critical z for a two-tailed test with alpha = 0.05.
+crit.diff <- crit.z * se  # Convert z to the critical difference.
 
-crit.diff <- crit.z * se  # Convert z to the critical difference
-
-
-
-library(TOSTER)  # Install TOSTER using devtools::install_github("Lakens/TOSTER"), or download TOSTtwo.prop.R from https://github.com/Lakens/TOSTER/tree/master/R and then source it. 
-
-
-#  We first load the data and exclude cases to only include cases that did not meet the authors' exclusion criteria.
+#  Load the data and exclude cases to only include cases that did not meet the authors' exclusion criteria.
 lynott.data <- read.csv("Example4_Lynott-et-al_data-cleaned.csv")
 lynott.data <- lynott.data[lynott.data$include1 == 1,]
 
-
-#  We then calculate the summary statistics from the data that we need in the TOST calculation below
+#  Calculate the summary statistics from the data that we need in the TOST calculation below.
 n.hot  <- sum(lynott.data$packtype == 1)
 n.cold <- sum(lynott.data$packtype == 0)
 selfish.hot  <- sum(lynott.data$selfish[lynott.data$packtype == 1] == 1, na.rm = TRUE) / n.hot
 selfish.cold <- sum(lynott.data$selfish[lynott.data$packtype == 0] == 1, na.rm = TRUE) / n.cold
 diff <- selfish.hot - selfish.cold
 
-#  We can now run the TOST!
+#  Calculate equivalence test for two proportions.
 Lynott <- TOSTtwo.prop(prop1 = selfish.cold,
                      prop2 = selfish.hot,
                      n1    = n.cold,
