c78a68e7af4e98f6cd9af4c2bbbe29d9d6207a60
Scheel <a.m.scheel@tue.nl>
2018-05-09 16:53:39 +0200

    Correlation value had the wrong sign (changed it to r = -0.04)

--- Example 5 (Kahane et al.)/Example5_simplified.R
+++ Example 5 (Kahane et al.)/Example5_simplified.R
@@ -13,7 +13,7 @@ library(TOSTER)
 # If you want to use this for your own data, simply edit the numbers in the function below to match your data.
 
 TOSTr(n = 231,  # Number of pairs of observations
-      r = 0.04206585,  # Correlation effect size
+      r = -0.04206585,  # Correlation effect size
       low_eqbound_r = -0.1830961,  # Value for the lower equivalence bound
       high_eqbound_r = 0.1830961,  # Value for the higher equivalence bound
       alpha = 0.05)  # Alpha level for TOST and NHST
